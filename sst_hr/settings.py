73b5bc3796acfe1dec9acf917a0d28a511297ac5
bjorne90 <bjce90@gmail.com>
2023-08-28 09:03:15 +0000

    Changes to home.html and database...

--- sst_hr/settings.py
+++ sst_hr/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dh3-9beq7by^twv^d!iz1f=5(y0pf2@+zdd%74o5myj4+zzqs1'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
